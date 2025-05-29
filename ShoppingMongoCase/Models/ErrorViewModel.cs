7ce8a6152cfca790f2db4b0fbcc39cd827120862
Kadir-Hali <kadirhali07@gmail.com>
2025-05-29 14:59:26 +0300

    Add project files.

---
+++ ShoppingMongoCase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingMongoCase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
