427f6d05b57487ac28e2d278fb3f3a01e9ec904c
Kadir-Hali <kadirhali07@gmail.com>
2025-06-18 17:17:05 +0300

    18.06.25

---
+++ ShoppingMongoCase/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingMongoCase.Dtos.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+        public int StockCount { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ ShoppingMongoCase/Services/ProductServices/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using ShoppingMongoCase.Dtos.ProductDtos;
+
+namespace ShoppingMongoCase.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetProductByIdDto> GetProductByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
