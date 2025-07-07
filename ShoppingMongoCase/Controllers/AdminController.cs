52ed33fb9a7e950a0aac838d0fc64e9e367af7a1
Kadir-Hali <kadirhali07@gmail.com>
2025-07-07 17:49:40 +0300

    07.07.25

---
+++ ShoppingMongoCase/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingMongoCase.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+
+    }
+}
